094c0dba2e6a8f75f24c15b745342432eae98d26
phyoko <pkkoedu777@gmail.com>
2023-10-19 00:33:17 +0630

    added loading ui

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <div className=" h-screen flex justify-center items-center">
+            <h1 className=" animate-pulse text-2xl mb-2 font-semibold">
+                Loading...
+            </h1>
+        </div>
+    )
+}
